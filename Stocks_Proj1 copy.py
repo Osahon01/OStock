0326ff7090c906c1a29c8d7477b0fb37a8015519
Osahon01 <88063479+Osahon01@users.noreply.github.com>
2021-07-28 10:58:38 -0400

    Update Stocks_Proj1 copy.py

--- Stocks_Proj1 copy.py
+++ Stocks_Proj1 copy.py
@@ -14,13 +14,12 @@ stock = y_fin.Ticker("ABEV3.SA")
 data = stock.info
 startDate = datetime.datetime(2021, 7, 1)
 today = date.today()
-print("Welcome to the OStock-bot! \n Menu overview: \n 1. Current stock price!! \n 2. Should I sell? \n 3. Should I buy? \n 4. Top stock gainers :) \n 5. Top stock losses :( ")
+print("Welcome to the OStock-bot! \n Menu overview: \n 1. Current stock price!! \n 2. Should I sell? \n 3. Should I invest in __ company? \n 4. Top stock gainers :) \n 5. Top stock losses :( ")
  
 print(" \n 1. Current stock price!! \n Please input the symbol for the stock you are inquiring about:")
 
 def get_current_price(symbol):
     ticker = y_fin.Ticker(symbol)
-    todays_data = ticker.history(period = '20mins')
     
     print(" \n Here is the current stock price for the company you were inquiring about:", "$", ticker.info["currentPrice"])
     print("\n")
@@ -32,7 +31,7 @@ def get_current_price(symbol):
         print("Please type in the symbol of the company: ")  
         symbol2 = input()
         
-        print("Please type the price you paid for the stock; express it as an integer value")         
+        print("Please type in the price you paid for the stock; express it as an integer value")         
         b_price = input()
         c_price = y_fin.Ticker(symbol2).info['currentPrice']
         g_l=(float(c_price) - float(b_price))
@@ -52,15 +51,16 @@ def get_current_price(symbol):
         print("Please type in the symbol of the company: ")  
         symbol3 = input()
         #print(y_fin.Ticker(symbol3).info)
-        per_change = y_fin.Ticker(symbol3).info['revenueGrowth']
-        if float(per_change) < 2:
-             print("OStock-bot does not advise buying this stock at the moment. {}".format(per_change))
+        q_change = y_fin.Ticker(symbol3).info['revenueGrowth']
+        per_change = (q_change*100)
+        if float(per_change) < 50:
+             print("OStock-bot does not advise buying this stock at the moment. Its current revenue growth rate is {}%".format(per_change))
         else:
-             print("OStock-bot advises you to sell to secure a profit, or allow the stock to continue to grow")            
+             print("OStock-bot advises you to invest to secure a profit before it's too late. Its current revenue growth rate is {}%".format(per_change))            
     elif response=='skip':
         print()
 
-          print("\n 4. Top stock gainers :) [run/skip]")
+    print("\n 4. Top stock gainers :) [run/skip]")
     response=input()
     if response=="run":
         win=stock_info.get_day_gainers()
